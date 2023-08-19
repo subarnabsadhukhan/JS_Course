957a17f8a450798ede0c7fbd181b7338bdfada95
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-19 22:06:46 +0530

    learn diff data types: boolean, undefined, null & typeof operator & dynamic typing

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,24 @@
-let firstName = "Subarnab"; // Here, firstName is a Variable.
-console.log(firstName);
-console.log(firstName);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// typeof :The typeof operator returns a string indicating the type of the operand's value.
+console.log(typeof true); // boolean
+console.log(typeof javascriptIsFun); // boolean
+console.log(typeof 22); // number
+console.log(typeof "Subarnab"); // string
+
+// dynamic typing
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun); // string
+
+// undefined type
+let year;
+console.log(year); // undefined (value of empty variable is undefined)
+console.log(typeof year); // undefined
+
+year = 2002;
+console.log(typeof year); // number
+
+//null data type
+console.log(null); // null
+console.log(typeof null); // object
