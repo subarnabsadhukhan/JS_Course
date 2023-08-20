b4e7d4c068b7e5d66d1171efc7a5bb81058ae6e2
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-20 08:52:04 +0530

    Use of Template String for a complex string with lots of Variables and write a multi-line string with Template Literals

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,25 +1,40 @@
-now = 2023;
-console.log(now - 1991 > now - 2015); //true
-// Operator Procedure: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence#table
+/////////////////////////////////////////////
+////////////// Strings and Template Literals
 
-//usually all the Math Operators are executed before Comparison Operators.
+const firstName = "Ishita";
+const job = "nursing";
+const birthYear = 2002;
+const currentYear = 2023;
 
-//////////////////
-console.log(20 - 5 - 4); //11  (left to right)
-/// console.log(2 ** 2 ** 3); (right to left)
-console.log(2 ** (2 ** 3)); //256
+const ishita =
+  "I'm " +
+  firstName +
+  ", I am " +
+  (currentYear - birthYear) +
+  " years old. I am studying " +
+  job +
+  " now."; // This way is painful!!
 
-////////// Assignment Operators are right-to-left
-let x, y;
-x = y = 10;
-console.log(x, y); //10 10
-//if left-to-right, then x will equal to y (x = y) and y will become undefined (y = undefined) because they are both defined as empty variable first.
+// JS can simply convert the number [(currentYear - birthYear)] to a string to join with other strings.
 
-////////////////////////
-// Grouping Operator (...)  has highest precedence.
-const ageJonas = 25;
-const ageSarah = 15;
-let averageAge = ageJonas + ageJonas / 2; // Division happens first in this case.
-console.log(averageAge); //37.5 (wrong)
-averageAge = (ageJonas + ageSarah) / 2;
-console.log(averageAge); //20 (correct)
+console.log(ishita);
+
+////////////// Template Literals
+
+const ishitaNew = `I'm ${firstName}, I am ${
+  currentYear - birthYear
+} years old. I am studying ${job} now. `; // using template literals
+
+console.log(ishitaNew);
+
+console.log(`Just a regular string in Template String.`);
+
+///////// Multi-line Strings with Template Strings
+
+//Normal Way
+console.log("first line \nsecond line \nthird line");
+
+//With Template Strings
+console.log(`first line
+second line
+third line`);
