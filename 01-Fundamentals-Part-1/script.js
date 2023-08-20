31880e4f146d46251dc0e1021849fe788aa9d693
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-20 08:06:44 +0530

    Operator Procedure list and left-to-right & right-to-left operator and Grouping Operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,33 +1,25 @@
-///// BASIC OPERATORS
+now = 2023;
+console.log(now - 1991 > now - 2015); //true
+// Operator Procedure: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence#table
 
-/// MATHEMATICAL OPERATORS
-const difference = 25 - 20;
-const addition = 20 + 2;
-console.log(difference, addition); //5 22
-console.log(addition * 10, addition / 10); //220  2.2
+//usually all the Math Operators are executed before Comparison Operators.
 
-// We can use Plus (+) Operators to Join/Concatenate multiple Strings.
-const firstName = "Subarnab";
-const lastName = "Sadhukhan";
-console.log(firstName + " " + lastName); //Subarnab Sadhukhan
+//////////////////
+console.log(20 - 5 - 4); //11  (left to right)
+/// console.log(2 ** 2 ** 3); (right to left)
+console.log(2 ** (2 ** 3)); //256
 
-/// typeof operator
-console.log(typeof lastName); //string
+////////// Assignment Operators are right-to-left
+let x, y;
+x = y = 10;
+console.log(x, y); //10 10
+//if left-to-right, then x will equal to y (x = y) and y will become undefined (y = undefined) because they are both defined as empty variable first.
 
-/// ASSIGNMENT (=) OPERATORS
-let x = 10 + 5;
-x += 4; // x = x + 4
-console.log(x); //19
-
-y = 5;
-y *= 2; // y = y * 2
-console.log(y); //10
-
-x++; // x = x + 1
-x--; //x = x - 1;
-x--; //x = x - 1;
-console.log(x); //18
-
-/// COMPARISON OPERATORS (> , < , >= , <=)
-console.log(x > y); //true
-console.log(y >= 20); //false
+////////////////////////
+// Grouping Operator (...)  has highest precedence.
+const ageJonas = 25;
+const ageSarah = 15;
+let averageAge = ageJonas + ageJonas / 2; // Division happens first in this case.
+console.log(averageAge); //37.5 (wrong)
+averageAge = (ageJonas + ageSarah) / 2;
+console.log(averageAge); //20 (correct)
