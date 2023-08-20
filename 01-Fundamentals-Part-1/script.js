8264d5a05ab31e98331a9cfb65280c111888eb72
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-20 18:42:27 +0530

    learn Boolean Logic, && and || operators combining with if else

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,66 +1,31 @@
-/////////////////////////////////////////////////
-////////// EQUALITY OPERATORS: == vs. ===
+//////////////////////////////////////////
+////////////////// Boolean Logic
 
-let age = 18;
+let hasDriversLicense = true;
+let hasGoodVision = true;
 
-// Strict Equality Operator (===): Does not perform type coertion
-if (age === 18) {
-  console.log(`You just became an Adult :D`); //console prints
-}
+// AND Operator (&&) and OR Operator (||)
 
-// Loose Equality Operator (===): Does perform type coertion
-if (age == 18) {
-  console.log(`You just became an Adult :)`); //console prints
-} // Same type. So, No type coertion needed.
+console.log(hasDriversLicense && hasGoodVision); //true
+console.log(hasDriversLicense || hasGoodVision); //true
 
-//---------------------------------------------
+hasGoodVision = false;
 
-const birthYear = "2018";
+console.log(hasDriversLicense && hasGoodVision); //false
+console.log(hasDriversLicense || hasGoodVision); //true
 
-// Strict Equality Operator (===): Does not perform type coertion
-if (birthYear === 2018) {
-  console.log(`You are not an Adult`); //No Execution
-}
+console.log(!hasDriversLicense); //false
+console.log(!hasGoodVision); //true
 
-// Loose Equality Operator (===): Does perform type coertion
-if (birthYear == 2018) {
-  console.log(`You are not an Adult :(`); //console prints
-} // Type Coertion Performed.
+//------------------------------------------------
+//Example:
 
-//---------------------------------------------
+hasDriversLicense = true;
+hasGoodVision = true;
+let isTired = true;
 
-// Example:
-console.log(18 == 18); //true
-console.log("18" == 18); //true
-console.log("18" === 18); //false
-console.log("18" === "18"); //true
-
-//-----------------------------------------------
-
-const favourite = prompt(`What's your favourite number?`);
-console.log(favourite);
-console.log(typeof favourite); //string
-
-//---------------------------------------------
-/////// More conditions with else if:
-const lottery = Number(prompt(`Choose a number:`));
-if (lottery === 22) {
-  console.log(`You have won the Jackpot :D`);
-} else if (lottery === 56) {
-  console.log(`You have won the 2nd Prize :)`);
-} else if (lottery === 99) {
-  console.log(`You have won the 3rd Prize :)`);
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("You should drive");
 } else {
-  console.log(`You have lost the Game :(`);
+  console.log("You should not drive"); // console prints
 }
-
-//////////////////////////////////////////////
-//////// Different Operator
-
-// Loose Different Opertor
-console.log(18 != 18); //false
-console.log("18" != 18); //false
-
-// Strict Different Operator
-console.log("18" !== 18); //true
-console.log("18" !== "18"); //false
