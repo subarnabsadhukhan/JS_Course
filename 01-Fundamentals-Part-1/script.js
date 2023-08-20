c695b7760f9a1b95af6f74d6960974bf2837d19e
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-20 12:22:16 +0530

    truly & falsy values in case of Boolaen

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,51 +1,56 @@
-//////////////////////////////////////////
-///////////// Type Conversion and Coercion
+////////////////// Truthy and Falsy Values ///////////////
 
-///// type conversion is when we manually convert from one type to another. On the other hand, type coercion is when JavaScript automatically converts types behind the scenes for us.
+//---------------------------------------
 
-//////////////////////////////////////////////
-//// type conversion
-const inputYear = "1991";
-console.log(inputYear + 20); //199120
-//It has basically concatenate the string.
+/*
+💡 falsy values are values that are not exactly false, but will become false when we try to convert them into a boolean.
 
-console.log(Number(inputYear)); // 1991
-console.log(typeof Number(inputYear)); // number
-console.log(Number(inputYear) + 20); //2011
+💡 5 falsy values are: 0, "", undefined, null, NaN
+💡 all of these five values will be converted to false when we attempt to convert them to a boolean.
+*/
+
+//---------------------------------------
+
+/*
+💡 any number that is not zero or any string that is not an empty string will be converted to true when we attempt to convert them to a boolean.
+*/
+
+//-----------------------------------------
+// Examples:
+console.log(Boolean(0)); //false
+console.log(Boolean(null)); //false
+console.log(Boolean("Ishita")); //true
+console.log(Boolean(" ")); //true (not an empty string)
+console.log(Boolean("")); //false
+console.log(Boolean({})); //true
+console.log(Boolean(55)); //true
+
+//-----------------------------------------
+// Use Case:
+
+const money = 0;
+if (money) {
+  console.log("Dont't spend it all");
+} else {
+  console.log("You should get a Job!");
+}
+//console print: You should get a Job!
+// as 0 is falsy when converted to Boolean, else statement triggered.
 
 //------------------------------------------
-//💡  to convert a string to a number, use Number() function.
+// Another Use Case:
 
-let myPhone = "samsung2020";
-console.log(Number(myPhone)); //NaN
-console.log(typeof NaN); //number
-// Actually NaN is invalid Number (Not a Number).
-console.log(typeof Number(myPhone)); //number
+let height;
+if (height) {
+  console.log("YAY! Height is defined!");
+} else {
+  console.log(`Height is UNDEFINED`); //console prints
+}
+// as height is not assigned any value or undefined, else statement will be triggered.
 
-//------------------------------------------
-//💡  to convert a number type to a string, use String() function.
-let myAge = 21;
-console.log(typeof myAge); //number
-console.log(String(myAge)); //21
-console.log(typeof String(myAge)); //string
-
-///////////////////////////////////////////////////
-//// type coercion
-console.log("I am " + myAge + " years old.");
-// if JS don't have type coercion, we have to write the above code:
-console.log("I am " + String(myAge) + " years old.");
-
-//------------------------------
-console.log("23" - 3 - "10"); //10
-// Here, in case of minus operator strings are converted to number.
-
-console.log("23" + 3 + "10"); //23310
-// Here, in case of plus operator, numbers are converted to string and concatenates.
-
-console.log("23" + 3 - "10"); //223
-console.log(typeof ("23" + 3 - "10")); //number
-
-// other cases....
-console.log("23" * "3"); //69
-console.log("23" / "3"); //7.666666666666667
-console.log(2 + 3 + 4 + "5"); //95
+let weight = "50kg";
+if (weight) {
+  console.log("YAY! weight is defined!"); //console prints
+} else {
+  console.log(`weight is UNDEFINED`);
+}
