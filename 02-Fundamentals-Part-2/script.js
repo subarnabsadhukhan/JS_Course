5f372fc9456eabf720a89dc5a6ce44e880c01e47
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-09-12 09:22:57 +0530

    Functions: how to declare a function?

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,28 +1,25 @@
-////////////////////////////////////////////
-////////// ACTIVATING STRICT MODE
 "use strict";
-/* 
 
-💚 we activated strict mode for the entire script. Now what's important is that this statement here basically has to be the very first statement in the script.
+//////////////////////////////////
+////////////// FUNCTIONS
 
-💚 So if we have any code before this, then strict mode will not be activated.
+function logger() {
+  console.log("My name is Subarnab");
+}
+//💚 Not all Functions need to return anything or need to accept parameter(s).
 
-💚 We actually can also activate strict mode, only for a specific function or a specific block. But I don't really see the point in doing that.
+// calling / running / invoking function
+logger();
+logger();
 
-💚 So again, first strict mode forbids us to do certain things and second it creates visible errors and the developer console, where in other situations JavaScript would just fail silently.
+//----------------- Example --------------
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
 
-💚 strict mode reserves some keywords so we cannot use them for variables. 
+console.log(fruitProcessor());
+//Console: Juice with undefined apples and undefined oranges.
 
-*/
-
-let hasDriversLicense = false;
-const passTest = true;
-
-// if (passTest) hasDriverLicense = true;
-// // Uncaught ReferenceError: hasDriverLicense is not defined
-if (hasDriversLicense) console.log(`I can Drive :D`);
-
-//----------------------------------------------------
-
-const private = "my secret";
-// // Uncaught SyntaxError: Unexpected strict mode reserved word. Here, private is reserved word.
+console.log(fruitProcessor(3, 2));
+//Console: Juice with 3 apples and 2 oranges.
