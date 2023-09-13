b1cccafadd8a8d2efc35a58c8dd64ea93e7a1eb0
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-09-13 08:29:20 +0530

    Dot vs. Bracket Notation:
    - getting a Property from an Object using Dot and Bracket Notation.
    - use Cases of Bracket Notation.
    - add Properties to an Object.

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,6 +1,6 @@
 "use strict";
 ////////////////////////////////////////////////
-///////// Introduction to Objects
+///////// Dot vs. Bracket Notation
 
 // Object Literal Syntax
 const Ishita = {
@@ -11,4 +11,53 @@ const Ishita = {
   friends: ["Disha", "Shreya", "Kuheli"],
 };
 
-//💚 each of these keys are also called property.
+console.log(Ishita);
+
+//////////////////////////////////////
+//-------- Getting a Property from an Object ----
+
+// Dot Notation
+console.log(Ishita.job); //nursing
+
+// Bracket Notation
+console.log(Ishita["job"]); //nursing
+
+// ---- use Cases of Bracket Notation -------------
+/* 
+
+💚 in bracket notation, we can actually put any expression that we'd like, so we don't have to explicitly write the string here, but instead we can compute it from some operation because remember that an operation is basically an expression.
+
+// Case 1 ----------------
+*/
+const nameKey = "Name";
+console.log(Ishita[`first` + nameKey]); //Ishita
+console.log(Ishita[`last` + nameKey]); //Ghosh
+
+// Case 2 -----------------
+
+const interestedIn = prompt(
+  "What do you want to know about Ishita? Choose between firstName, lastName, age, job, and friends"
+);
+
+if (Ishita[interestedIn]) {
+  console.log(Ishita[interestedIn]);
+} else {
+  console.log(
+    `Wrong Request! Choose between firstName, lastName, age, job, and friends`
+  );
+}
+
+//////////////////////////////////////////////
+/////// Add Properties to an Object
+
+Ishita.location = "Kolkata";
+Ishita["sex"] = "Female";
+
+console.log(Ishita);
+
+// --------- Challenge ----------
+console.log(
+  `${Ishita.firstName} has ${Ishita.friends.length} friends, and ${
+    Ishita.sex === "Female" ? "her" : "his"
+  } best friend is called ${Ishita.friends[0]}.`
+); //Ishita has 3 friends, and her best friend is called Disha.
