39d2a5c55e9cb8b2aaf68f78141ea56bc5036720
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-09-18 20:28:44 +0530

    PROJECT #1: Guess My Number! - Completed

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # JavaScript in the Browser: DOM and Events Fundamentals
+
+## PROJECT #1: Guess My Number!
+
+### How to Select an Element from Web Page?
+
+    console.log(document.querySelector(".message"));
+
+    console.log(document.querySelector(".message").textContent);
+    //Start guessing...
+
+### What's the DOM and DOM Manipulation
+
+> **document** is a special **object** that we have access to in JavaScript. And this object serves as an entry point into the DOM.
+>
+> The rule is that whatever is in the HTML document also has to be in the **DOM**. And so, the DOM really is a complete representation of the HTML document.
+>
+> Follow this [PDF](./readme-assets/DOM.pdf).
+
+### Selecting and Manipulating Elements
+
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+    console.log(document.querySelector(".message").textContent);
+    //🎉 Correct Number!
+
+### Handling Click Event
+
+    document.querySelector(".guess").addEventListener("click", function () {
+        // Function Body
+    });
+
+### Manipulating CSS Styles
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+- use `.style` after selecting elements.
+- for CSS styles with more than 1 word (ex. `background-color`), use camelCase version in JS.
+- write property values inside `" "`.
--- script.js
+++ script.js
@@ -1,1 +1,104 @@
 "use strict";
+
+///// Learinging Area /////
+/*
+
+//---- How to Select an Element? ----
+console.log(document.querySelector(".message"));
+
+console.log(document.querySelector(".message").textContent); //Start guessing...
+
+// --- Selecting and Manipulating Elements ----
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+console.log(document.querySelector(".message").textContent); //🎉 Correct Number!
+
+document.querySelector(".number").textContent = 23;
+
+*/
+/////////////////////////////////////////////////
+///// code of PROJECT #1: Guess My Number! /////
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 10;
+let highScore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector(".score").textContent = score;
+
+      document.querySelector(".message").textContent = "📈 Too High!";
+    } else {
+      score--;
+      document.querySelector(".score").textContent = 0;
+      document.querySelector(".message").textContent =
+        "👎🏻 You Lost the Game. Try Again!";
+      document.querySelector("body").style.backgroundColor = "#b34747";
+      document.querySelector(".number").textContent = secretNumber;
+      document.querySelector(".number").style.width = "30rem";
+      document.querySelector(".check").disabled = true;
+      document.querySelector(".guess").readOnly = true;
+      document.querySelector(".check").style.color = "#8f8d8d";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector(".score").textContent = score;
+
+      document.querySelector(".message").textContent = "📉 Too Low!";
+    } else {
+      score--;
+      document.querySelector(".score").textContent = 0;
+      document.querySelector(".message").textContent =
+        "👎🏻 You Lost the Game. Try Again!";
+      document.querySelector("body").style.backgroundColor = "#b34747";
+      document.querySelector(".number").textContent = secretNumber;
+      document.querySelector(".number").style.width = "30rem";
+      document.querySelector(".check").disabled = true;
+      document.querySelector(".guess").readOnly = true;
+      document.querySelector(".check").style.color = "#8f8d8d";
+    }
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".check").disabled = true;
+    document.querySelector(".guess").readOnly = true;
+    document.querySelector(".check").style.color = "#8f8d8d";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  }
+});
+
+document.querySelector(".guess").addEventListener("click", function () {
+  if (
+    score > 0 &&
+    Number(document.querySelector(".guess").value) !== secretNumber
+  ) {
+    document.querySelector(".guess").value = "";
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 10;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".guess").readOnly = false;
+  document.querySelector(".check").disabled = false;
+  document.querySelector(".check").style.color = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+});
