9d7e869b4ec6d09981f171d430782531afff7430
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-10-17 08:45:37 +0530

    Nested Object Destructuring

--- script.js
+++ script.js
@@ -25,58 +25,9 @@ const restaurant = {
   },
 };
 
-const { name, openingHours, categories } = restaurant;
-console.log(name);
-console.log(openingHours);
-console.log(categories);
-
-//=============================
-
-//variable names to be different from the property names
+////// Nested Object Destructuring
 const {
-  name: resturantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
+  fri: { open: openTime, close: closeTime },
+} = restaurant.openingHours;
 
-console.log(resturantName); //Classico Italiano
-
-console.log(hours);
-// //CONSOLE:
-// {
-//     thu: { open: 12, close: 22 },
-//     fri: { open: 11, close: 23 },
-//     sat: { open: 0, close: 24 }
-//   }
-
-console.log(tags);
-//CONSOLE: [ 'Italian', 'Pizzeria', 'Vegetarian', 'Organic' ]
-
-//=============================
-
-// Setting Default value to variables
-
-const { menu = [], starterMenu: starters = [] } = restaurant;
-
-console.log(menu); //[]
-
-console.log(starters);
-//CONSOLE: [ 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad' ]
-
-////////============================
-
-// Mutating Variables
-let a = 111;
-let b = 222;
-const obj = { a: 21, b: 29, c: 99 };
-
-/*
-{a,b} = obj
-console.log(a,b); //SyntaxError: Unexpected token '='
-
-REASON☝🏻: When we start a line with a curly brace like above, then JavaScript expects a code block. And since we cannot assign anything to a code block, we get this error.
-*/
-
-// Correct Way
-({ a, b } = obj);
-console.log(a, b); //21 29
+console.log(openTime, closeTime); //11 23
