520b19bb9b3b78f1456ef77fc1564c61e30339a3
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-10-17 09:26:50 +0530

    use cases of spread operators

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 "use strict";
 
-///////////////////////////////
-//////// Destructuring Objects
+////////////////////////////////
+//// The Spread Operator ( … )
 
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
+//we can use the spread operator to basically expand an array into all its elements. So basically unpacking all the array elements at one.
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery: function ({ time, address, mainIndex = 0, starterIndex = 1 }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-};
+const arr = [7, 8, 9];
 
-restaurant.orderDelivery({
-  time: "22:30",
-  address: "kolkata",
-  mainIndex: 2,
-  starterIndex: 1,
-});
-//CONSOLE: Order received! Bruschetta and Risotto will be delivered to kolkata at 22:30
+const arr2 = [1, 2, arr];
+console.log(arr2); //[ 1, 2, [ 7, 8, 9 ] ]
 
-restaurant.orderDelivery({
-  time: "22:30",
-  address: "kolkata",
-});
-//CONSOLE: Order received! Bruschetta and Pizza will be delivered to kolkata at 22:30
+// Using Spread Operator
+const newArr = [1, 2, ...arr];
+console.log(newArr); //[ 1, 2, 7, 8, 9 ]
+
+//when we need to pass multiple elements into a function
+console.log(1, 2, 7, 8, 9); //1 2 7 8 9
+console.log(...newArr); //1 2 7 8 9
+
+/////////////////////////////////////////////////
+// Spread operator takes all the elements from the array and it also doesn't create new variables. And as a consequence, we can only use it in places where we would otherwise write values separated by commas.
+
+////////////////////////////////////////////////
+/////// Use Cases of Spread Operators
+
+// Creating Shallow Copy of Arrays
+const arrCopy = [...arr];
+console.log(arrCopy); //[ 7, 8, 9 ]
+
+// Join 2 arrays
+const joinArr = [...arr, ...newArr];
+console.log(joinArr); //[ 7, 8, 9, 1, 2, 7, 8, 9 ]
