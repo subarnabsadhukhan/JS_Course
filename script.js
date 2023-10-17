bc614a61e0dee906ca650991defcdd87203f79a1
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-10-17 09:34:58 +0530

    Using Spread Operator on Strings

--- script.js
+++ script.js
@@ -3,31 +3,23 @@
 ////////////////////////////////
 //// The Spread Operator ( … )
 
-//we can use the spread operator to basically expand an array into all its elements. So basically unpacking all the array elements at one.
+// Use case on String
+const str = "Subarnab";
+const letters = [...str];
 
-const arr = [7, 8, 9];
+console.log(letters);
+/*
+CONSOLE:
+[
+  'S', 'u', 'b',
+  'a', 'r', 'n',
+  'a', 'b'
+] 
+*/
 
-const arr2 = [1, 2, arr];
-console.log(arr2); //[ 1, 2, [ 7, 8, 9 ] ]
+console.log(...str); //S u b a r n a b
+console.log("s", "u", "b"); //s u b
 
-// Using Spread Operator
-const newArr = [1, 2, ...arr];
-console.log(newArr); //[ 1, 2, 7, 8, 9 ]
-
-//when we need to pass multiple elements into a function
-console.log(1, 2, 7, 8, 9); //1 2 7 8 9
-console.log(...newArr); //1 2 7 8 9
-
-/////////////////////////////////////////////////
-// Spread operator takes all the elements from the array and it also doesn't create new variables. And as a consequence, we can only use it in places where we would otherwise write values separated by commas.
-
-////////////////////////////////////////////////
-/////// Use Cases of Spread Operators
-
-// Creating Shallow Copy of Arrays
-const arrCopy = [...arr];
-console.log(arrCopy); //[ 7, 8, 9 ]
-
-// Join 2 arrays
-const joinArr = [...arr, ...newArr];
-console.log(joinArr); //[ 7, 8, 9, 1, 2, 7, 8, 9 ]
+/////////////////////
+// Multiple values separated by a comma are usually only expected when we pass arguments into a function, or when we build a new array.
+////////////////////
