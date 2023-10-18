9d8db329b8fbf615b2dceb27b60edb2ed91c6740
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-10-18 09:58:19 +0530

    Coding Challenge #2

--- script.js
+++ script.js
@@ -1,71 +1,104 @@
 "use strict";
 
 ////////////////////////////////////////
-/////// Looping Objects: Object Keys, Values, and Entries
+// Coding Challenge #2
 
-const openingHours = {
-  thu: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0, // Open 24 hours
-    close: 24,
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
 
-///////////////////////
-// Creating an Array of Property Names of an Object
-const properties = Object.keys(openingHours);
-console.log(properties); //[ 'thu', 'fri', 'sat' ]
-
-////////////////////////
-// Looping over Property Names
-for (const day of Object.keys(openingHours)) console.log(day);
-
-//CONSOLE:
-// thu
-// fri
-// sat
-
-//////////////////////////
-// Creating an Array of Property values of an Object
-const values = Object.values(openingHours);
-console.log(values);
-
-/*
-CONSOLE:
-[
-  { open: 12, close: 22 },
-  { open: 11, close: 23 },
-  { open: 0, close: 24 }
-]
-*/
-/////////////////////////
-// Creating an Array of Object key-values
-const entries = Object.entries(openingHours);
-console.log(entries);
+////////////1.
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
 /* 
 CONSOLE:
-[
-  [ 'thu', { open: 12, close: 22 } ],
-  [ 'fri', { open: 11, close: 23 } ],
-  [ 'sat', { open: 0, close: 24 } ]
-]
+Goal 1: Lewandowski
+Goal 2: Gnarby
+Goal 3: Lewandowski
+Goal 4: Hummels
 */
 
-/////////////////////////////////
-// Exercise
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close}.`);
+////////////2.
+let sum = 0;
+const odds = Object.values(game.odds);
+for (const i of odds) sum += i;
+
+const average = sum / odds.length;
+console.log(average); //3.6933333333333334
+
+////////////3.
+for (const [team, i] of Object.entries(game.odds)) {
+  console.log(`Odd of ${game[team] ?? `draw`}: ${i}`);
 }
 /*
 CONSOLE:
-On thu we open at 12 and close at 22.
-On fri we open at 11 and close at 23.
-On sat we open at 0 and close at 24.
+Odd of Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of Borrussia Dortmund: 6.5
 */
+
+////////////4.
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers); //{ Lewandowski: 2, Gnarby: 1, Hummels: 1 }
