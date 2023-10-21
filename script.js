1f9330c01fcb2d6d2e9510ac197605303f0d8f29
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-10-21 08:39:12 +0530

    Functions Accepting Callback Functions

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 "use strict";
 
 ///////////////////////////////////
-// How Passing Arguments Works: Values vs. Reference
-const flight = "LH234";
-const jonas = {
-  name: "Jonas Schmedtmann",
-  passport: 24739479284,
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
 };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = "LH999";
-  passenger.name = "Mr. " + passenger.name;
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
 };
 
-checkIn(flight, jonas);
-console.log(flight); //LH234
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
 
-console.log(jonas);
-//{ name: 'Mr. Jonas Schmedtmann', passport: 24739479284 }
-//// WTF? ☝🏻
+  console.log(`Transformed by: ${fn.name}`);
+};
 
+transformer("JavaScript is the best!", upperFirstWord);
+//////////////////////// CallBack Function ☝🏻
 /*
-📌 JavaScript does not have `passing by reference`, only `passing by value`, even though it looks like it's passing by reference.
+CONSOLE:
+Original string: JavaScript is the best!
+Transformed string: JAVASCRIPT is the best!
+Transformed by: upperFirstWord
+*/
 
-So there are languages like C++, where you can pass a reference to any value, instead of the value itself. This works even with primitives.
+transformer("JavaScript is the best!", oneWord);
+///////////////////// CallBack Function ☝🏻
+/*
+CONSOLE:
+Original string: JavaScript is the best!
+Transformed string: javascriptisthebest!
+Transformed by: oneWord
 */
